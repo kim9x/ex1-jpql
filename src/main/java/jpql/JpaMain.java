9ed8a3dc7966189e2d1ab531c1e238a5420d12cb
Kim9xd <kim9xd@gmail.com>
2021-12-15 23:55:36 +0900

    * 객체지향 쿼리 언어1 - 기본 문법   - 페치 조인 1 - 기본

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -22,55 +22,63 @@ public class JpaMain {
 		tx.begin() ;
 		
 		try {
-			Team team = new Team();
-			team.setName("teamA");
-			em.persist(team);
+			Team teamA = new Team();
+			teamA.setName("팀A");
+			em.persist(teamA);
+			
+			Team teamB = new Team();
+			teamB.setName("팀B");
+			em.persist(teamB);
 			
 			Member member1 = new Member();
-			member1.setUsername("관리자1");
+			member1.setUsername("회원1");
 			member1.setAge(10);
 			member1.setType(MemberType.ADMIN);
-			member1.setTeam(team);
+			member1.setTeam(teamA);
 			em.persist(member1);
 			
 			Member member2 = new Member();
-			member2.setUsername("관리자2");
+			member2.setUsername("회원2");
 			member2.setAge(10);
 			member2.setType(MemberType.ADMIN);
-			member2.setTeam(team);
+			member2.setTeam(teamA);
 			em.persist(member2);
+			
+			Member member3 = new Member();
+			member3.setUsername("회원3");
+			member3.setAge(10);
+			member3.setType(MemberType.ADMIN);
+			member3.setTeam(teamB);
+			em.persist(member3);
 			
 			
 			em.flush();
 			em.clear();
-			
-//			String query = "select " +
-//								"case when m.age <= 10 then '학생요금' " +
-//								"	  when m.age >= 60 then '경로요금' " +
-//								"	  else '일반요금' " +
-//								"end " +
-//						   "from Member m";
-//			
-//			List<String> result = em.createQuery(query, String.class)
-//					.getResultList();
-//			
-//			for (String s : result) {
-//				System.out.println("s = " + s);
-//			}
-			
-//			String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
-			
-//			String query = "select nullif(m.username, '관리자') from Member m";
-			
-			String query = "select function('group_concat', m.username) from Member m";
+						
+//			String query = "select m from Member m join fetch m.team";
+			String query = "select distinct t from Team t join fetch t.members";
 
-			List<String> result = em.createQuery(query, String.class)
+//			List<Member> result = em.createQuery(query, Member.class)
+//					.getResultList();
+			
+			List<Team> result = em.createQuery(query, Team.class)
 					.getResultList();
 			
-			for (String s : result) {
-				System.out.println("s = " + s);
-			}
+//			for (Member member : result) {
+//				System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+//				// 회원1, 팀A(SQL)
+//				// 회원2, 팀A(1차캐시)
+//				// 회원3, 팀B(SQL)
+//				
+//				// 회원 100명 -> N + 1
+//			}
 			
+			for (Team team : result) {
+				System.out.println("team = " + team.getName() + ", " + team.getMembers().size());
+				for ( Member member : team.getMembers() ) {
+					System.out.println("-> member = " + member);
+				}
+			}
 			
 			tx.commit();
 			
@@ -78,7 +86,7 @@ public class JpaMain {
 			// TODO: handle exception
 			tx.rollback();	
 			e.printStackTrace();
-		} finally {
+		} finally { 
 			emf.close();
 		}
 		 
