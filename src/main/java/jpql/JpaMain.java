48a7d71933f60cb341d0521256161516a2946e1c
Kim9xd <kim9xd@gmail.com>
2021-12-16 03:37:49 +0900

    * 객체지향 쿼리 언어2 - 중급 문법   - 벌크 연산

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -55,14 +55,24 @@ public class JpaMain {
 			em.flush();
 			em.clear();
 			
-			List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-					.setParameter("username", "회원1")
-					.getResultList();
+			// 아래의 벌크 연산 쿼리를 사용해도 영속성 컨텍스트에 있는
+			// 모든 값을 변경해주지 못한다.
+			// JPA가 그 정도로 똑똑하지 않다.. ㅠㅠ
+			int resultCount = em.createQuery("update Member m set m.age = 20")
+					.executeUpdate();
+			em.clear();
 			
-			for(Member member : resultList) {
-				System.out.println("member = " + member);
-			}
+			// 값을 새로 가져오므로 20으로 변경된 데이터를 가져온다.
+			Member findMember = em.find(Member.class, member1.getId());
+			System.out.println("findMember = " + findMember);
 			
+			System.out.println("resultCount = " + resultCount);
+			
+			// 아래 값들은 age가 10로 설정되어 있다.
+			// 설명은 58~60 라인 참고.!
+			System.out.println("member1.getAge() = " + member1.getAge());
+			System.out.println("member2.getAge() = " + member2.getAge());
+			System.out.println("member3.getAge() = " + member3.getAge());
 			
 			tx.commit();
 			
